25da53f4bee0a6892cd8300d41815cd8eeec15e9
zxy2012 <617944292@qq.com>
2018-06-07 17:51:37 +0800

    the first commit

---
+++ src/problem15/test15.java
@@ -0,0 +1,26 @@
+package problem15;
+
+import org.junit.Test;
+import problem14.MaxCutting;
+
+
+/**
+ * Created by Xueyong on 2018/6/7.
+ */
+public class test15 {
+    MaxCutting mc = new MaxCutting();
+
+    @Test
+    public void test() {
+        // 功能测试：绳子长度 = 10
+        System.out.println("绳子长度 = 10 的最大值:"+mc.maxCutting(10));
+
+        // 边界值测试：绳子长度 = 0、1、2、3、4
+        for(int i=0;i<5;i++){
+            System.out.println("长度 = "+i+"的最大值:"+mc.maxCutting(i));
+        }
+    }
+}
+
+
+
